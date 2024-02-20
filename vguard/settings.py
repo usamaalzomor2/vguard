2c8d2e583b677ea8e2d5fbb7e68576257855a5b1
Usamaalzomor <osama.zomor2010@gmail.com>
2024-02-20 21:08:47 +0200

    feat: add some settings for deplyment

--- vguard/settings.py
+++ vguard/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b+eprz*o_-3#lr=qpi8esbu*=wdn5_s-xtqf5u_@)4_rv8ouo!'
+# SECRET_KEY = 'django-insecure-b+eprz*o_-3#lr=qpi8esbu*=wdn5_s-xtqf5u_@)4_rv8ouo!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,16 +80,25 @@ WSGI_APPLICATION = 'vguard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'vguard',
-       'USER': 'postgres',
-       'PASSWORD': 'psqlpassword',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'vguard',
+#        'USER': 'postgres',
+#        'PASSWORD': 'psqlpassword',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
+
+DATABASES = {  
+    'default': dj_database_url.config('DATABASE_URL')  
 }
+
+SECRET_KEY =  os.environ['SECRET_KEY']
+
+
+ALLOWED_HOSTS = ['vguard-production.up.railway.app',]
 
 
 # Password validation
@@ -126,7 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -157,3 +165,9 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
